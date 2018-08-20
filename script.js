562af9fb392300c293eac473f7e26547cda387a9
DevDelvo <kevinbdelvo@gmail.com>
2018-08-20 00:04:47 -0400

    created simple D3 chart with dummy data. Added normalize.css

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-var svgWidth = 500;
-var svgHeight = 300;
+let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svg = d3.select('svg')
+let svgWidth = 500;
+let svgHeight = 300;
+var barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+let svg = d3.select('svg')
     .attr("width", svgWidth)
-    .attr("height". svgHeight)
-    .attr("class", "bar-chart");
+    .attr("height", svgHeight)
+    // .attr("class", "bar-chart");
+
+let barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHeight - d;
+    })
+    .attr("height", function(d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+        let translate = [barWidth * i, 0];
+        return "translate(" + translate +")";
+    });
